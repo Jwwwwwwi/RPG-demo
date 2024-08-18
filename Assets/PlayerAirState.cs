ea15ee810893d9e0029603f19902641f7c352f28
Jwwwwwwi <936516942@qq.com>
2024-08-18 19:41:52 +0800

    init

---
+++ Assets/PlayerAirState.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAirState : PlayerState
+{
+    public PlayerAirState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (player.ISGroundDetected())
+        {
+            stateMachine.ChangeState(player.moveState);
+        }
+        
+        if (xInput != 0)
+        {
+            player.SetVelocity(xInput * player.moveSpeed * .8f, rb.velocity.y);
+        }
+
+        if (player.ISWallDetected())
+        {
+            stateMachine.ChangeState(player.wallSlideState);
+        }
+    }
+
+}
